7ab5d06a560d5350892a4ee24554e51c319d36db
shreyasarjun <shreyas.arjun007@gmail.com>
2021-05-29 17:19:56 +0530

    Revert "Level3"
    This reverts commit 704b00085959bb4522cc906946fe8208193c67a9.

--- Subtraction.py
+++ Subtraction.py
@@ -1,4 +1,7 @@
 print("India")
 print("Srilanka")
+<<<<<<< HEAD
 print("England")
 print("USA")
+=======
+>>>>>>> parent of 704b000 (Level3)
--- mathsoperation.py
+++ mathsoperation.py
@@ -1,4 +1,8 @@
 print("Bangalore")
+<<<<<<< HEAD
 print("Mumbai")
 print("Delhi")
 print("Chennai")
+=======
+print("Mumbai")
+>>>>>>> parent of 704b000 (Level3)
