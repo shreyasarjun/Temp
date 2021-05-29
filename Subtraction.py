eca20694427dd8f547122d56242851cbce2f09c6
shreyasarjun <shreyas.arjun007@gmail.com>
2021-05-29 17:53:39 +0530

    conflic resolve

--- Subtraction.py
+++ Subtraction.py
@@ -1,7 +1,4 @@
 print("India")
 print("Srilanka")
-<<<<<<< HEAD
 print("England")
 print("USA")
-=======
->>>>>>> parent of 704b000 (Level3)
--- mathsoperation.py
+++ mathsoperation.py
@@ -1,8 +1,5 @@
 print("Bangalore")
-<<<<<<< HEAD
 print("Mumbai")
 print("Delhi")
 print("Chennai")
-=======
 print("Mumbai")
->>>>>>> parent of 704b000 (Level3)
